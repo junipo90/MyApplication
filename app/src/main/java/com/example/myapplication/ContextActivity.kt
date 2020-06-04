76726efd705f93eaf90bd409bb6bb29c452f3472
이읍준 <66265293+junipo90@users.noreply.github.com>
2020-06-04 14:09:58 +0900

    study

--- app/src/main/java/com/example/myapplication/ContextActivity.kt
+++ app/src/main/java/com/example/myapplication/ContextActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_context.*
 
 class ContextActivity : AppCompatActivity() {
 
@@ -12,6 +15,30 @@ class ContextActivity : AppCompatActivity() {
         // Context 의 두 가지 종류
         val context: ContextActivity = this
         val applicationContext = getApplicationContext()
+
+        photoView.setOnClickListener(object : View.OnClickListener{
+            var isDefault = true
+            override fun onClick(v: View?) {
+                isDefault = !isDefault
+                if(isDefault){
+                    photoView.setImageResource(R.drawable.dog)
+                }else{
+                    photoView.setImageResource(R.drawable.gradient)
+                }
+            }
+        })
+
+        scaleTypeButton.setOnClickListener(object :View.OnClickListener{
+            var isDefault = true
+            override fun onClick(v: View?) {
+                isDefault = !isDefault
+                if(isDefault){
+                    photoView.scaleType = ImageView.ScaleType.CENTER_CROP
+                }else{
+                    photoView.scaleType = ImageView.ScaleType.FIT_XY
+                }
+            }
+        })
     }
     ///////
 }
